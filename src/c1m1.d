8799c81ef0f407b41f8f3389a06c9c4ec7032a92
Ilyas <yahiaaissailyas@gmail.com>
2020-04-25 19:12:26 +0200

    last modif

---
+++ src/c1m1.d
@@ -0,0 +1,10 @@
+c1m1.out: ../include/platform.h ../include/msp432p401r.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h \
+ ../include/msp_compatibility.h ../include/core_cm4.h \
+ ../include/core_cmInstr.h ../include/cmsis_gcc.h \
+ ../include/core_cmFunc.h ../include/core_cmSimd.h \
+ ../include/system_msp432p401r.h
